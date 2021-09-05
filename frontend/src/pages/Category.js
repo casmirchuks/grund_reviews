fc3de27eb005646837b165cf6d3b4ec836ff8dfa
Casmir Chuks <casmirakachi@gmail.com>
2021-09-05 06:59:11 +0200

    fixed backend dependency

--- frontend/src/pages/Category.js
+++ frontend/src/pages/Category.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function Category() {
+function Category() {
     return (
         <div>
             Category
         </div>
     )
 }
+
+
+export default Category;
